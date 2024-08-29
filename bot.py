96080b857aa12bb1500b4412f341ecb5a756959f
Pranay12378 <150745494+Pranay12378@users.noreply.github.com>
2024-08-29 18:21:50 +0100

    Add files via upload
    Discord meme bot. I have deleted the token for security reasons but the bot works fine with it!

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+
+class MyClient(discord.Client):
+    async def on_read(self):
+        print(f'Logged on as {self.user}')
+
+    async def on_message(self, message):
+      if message.author == self.user:
+        return
+
+      if message.content.startswith('!meme'):
+        await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('CODEHRECODEHERECODEERETOKENHERE')
+
+#! I have cleared the token for security purposes however the bot does work.
